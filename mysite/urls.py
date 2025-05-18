d51bcd29585b9f4e94521e67f46cc9b4143eb89b
Seu Nome <voce@exemplo.com>
2025-05-18 15:29:58 -0300

    Changed the HTML for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,14 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.urls import path
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path ('', include('blog.urls')),
 ]
 
-urlpatterns = [
-    path('', views.post_list, name='post_list'),
-]
+
